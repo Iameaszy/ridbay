3b5cfb1528f0aa809ec6d80682910fa069910443
Yusuf <iameaszy>
2018-12-15 14:27:53 +0000

    mend

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import React from 'react';
 import DefaultLayout from './layouts/home.layout';
 import { createBrowserHistory } from 'history';
 import { ConnectedRouter } from 'connected-react-router';
+import {Route} from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login-form/login';
 const history = createBrowserHistory();
 export default () => (
   <ConnectedRouter history={history}>
     <div>
-      <DefaultLayout exact path="/" component={Home} />
+      <Route exact path="/" component={Login} />
       <DefaultLayout exact path="/login" component={Login} />
     </div>
   </ConnectedRouter>
